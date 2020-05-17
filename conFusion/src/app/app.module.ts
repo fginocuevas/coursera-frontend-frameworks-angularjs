00c941ff0950df8b8c5a304c05e423f7890589c1
Gino <neodracarys@gmail.com>
2020-05-17 23:06:25 +0800

    Week 2 Angular Service Basics

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -11,11 +11,15 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule} from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule} from "@angular/material/button";
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import { DishService} from "./services/dish.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +31,9 @@ import { MatButtonModule} from "@angular/material/button";
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
